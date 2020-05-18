e6dad8fe1f93d7a7634bd6e2bdb711cd27d16657
Manudatta G <manudatta4u@gmail.com>
2020-05-18 18:08:26 +0530

    Update tictactoe.py

--- code/tictactoe.py
+++ code/tictactoe.py
@@ -1,3 +1,20 @@
+"""
+Tic-Tac-Toe is game where two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. 
+The game is designed in such a way that the player enters a number between [1-9] and the game board will be displayed on screen. At the end of the game system will display the winner.
+
+Game rules:
+1. Player 1 plays for [X] --- Player 2 plays for [O]
+2. Player enters the number between [1-9] to mark their position
+3. If X succeeds in placing three of their marks in a row (up, down, across, or diagonally), then X is the winner.
+4. If O succeeds in placing three of their marks in a row (up, down, across, or diagonally), then O is the winner.
+5. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.
+
+Conditions:
+1. You cannot enter the number apart from [1-9] to mark the position. System displays "Please enter the position between [1-9]" message.
+2. You cannot fill the position which has already been filled. System displays "Position has been marked already" message.
+"""
+
+
 import os    
 import time    
     
